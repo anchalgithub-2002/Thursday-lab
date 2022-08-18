f4bf4e1878c4816e6ba3cccaebf7fcdb896eb4dd
anchalgithub-2002 <109715107+anchalgithub-2002@users.noreply.github.com>
2022-08-18 18:39:10 +0530

    Add files via upload

---
+++ Count.java
@@ -0,0 +1,27 @@
+//to count the vowels and throw checked exception
+public class Count
+{
+	public int count(char[] CharArr)
+	for(char ch:charArr)
+	{
+		if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u') {
+			count++;
+		}
+		try {
+			if(ch=='x') {
+				System.out.println("Checked Exception");
+			}
+			catch(Exception e) {
+				System.out.println(e);
+			}
+		}
+		return count;
+	}
+	public static void main(String args[])
+	{
+		Count c=new Count();
+		char[] CharArray={'a','b','c','d','e','x'};
+		int countValueOfVowel=obj.countVowels(CharArray);
+		System.out.println(countValueOfVowel);
+	}	
+}
